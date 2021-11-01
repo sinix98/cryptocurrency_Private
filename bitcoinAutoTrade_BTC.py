d1514c8265a6affcb9a0c23c944729104a542904
Minjae Jeong <83259905+sinix98@users.noreply.github.com>
2021-11-01 16:16:42 +0900

    Update bitcoinAutoTrade_BTC.py

--- bitcoinAutoTrade_BTC.py
+++ bitcoinAutoTrade_BTC.py
@@ -5,7 +5,7 @@ import datetime
 access = "Gw30DxqwzoiKsGT5FjxxDx5kiMCCIirxIUsQjRxD"
 secret = "EBOnomYlT7KSl14d5CXPhyghu1ICSy7oGLPu5djC"
 
-def get_target_price(ticker, k):
+def get_target_price(ticker, k=0.5):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
